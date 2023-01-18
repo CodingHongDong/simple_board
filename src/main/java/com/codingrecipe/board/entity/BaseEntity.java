aec4add94b58bf99dd7dcecce8d303ee065c960c
HongDongHwan <hongdh94@gmail.com>
2023-01-18 13:17:33 +0900

    ADD simple board CRUD 구현

---
+++ src/main/java/com/codingrecipe/board/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.codingrecipe.board.entity;
+
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class BaseEntity {
+
+    @CreationTimestamp
+    @Column(updatable = false) // 수정 시에는 관여 안함
+    private LocalDateTime createdTime;
+
+    @UpdateTimestamp
+    @Column(insertable = false) // 입력 시에는 관여 안함
+    private LocalDateTime updatedTime;
+
+}
---
+++ src/main/java/com/codingrecipe/board/entity/BoardEntity.java
@@ -0,0 +1,56 @@
+package com.codingrecipe.board.entity;
+
+import com.codingrecipe.board.dto.BoardDTO;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+// DB의 테이블 역할을 하는 클래스
+@Entity
+@Getter
+@Setter
+@Table(name = "board_table")
+public class BoardEntity extends BaseEntity{
+
+    @Id // pk 컬럼 지정. 필수
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment !
+    private Long id;
+
+    @Column(length = 20, nullable = false) // 크기 20, not null 지정
+    private String boardWriter;
+
+    @Column // 디폴트 : 크기255, null가능
+    private String boardPass;
+
+    @Column
+    private String boardTitle;
+
+    @Column
+    private String boardContents;
+
+    @Column
+    private int boardHits;
+
+    public static BoardEntity toSaveEntity(BoardDTO boardDTO) {
+        // Entity 객체로 옮겨담는 작업이라고 생각하자
+        BoardEntity boardEntity = new BoardEntity();
+        boardEntity.setBoardWriter(boardDTO.getBoardWriter());
+        boardEntity.setBoardPass(boardDTO.getBoardPass());
+        boardEntity.setBoardTitle(boardDTO.getBoardTitle());
+        boardEntity.setBoardContents(boardDTO.getBoardContents());
+        boardEntity.setBoardHits(0);
+        return boardEntity;
+    }
+
+    public static BoardEntity toUpdateEntity(BoardDTO boardDTO) {
+        BoardEntity boardEntity = new BoardEntity();
+        boardEntity.setId(boardDTO.getId());
+        boardEntity.setBoardWriter(boardDTO.getBoardWriter());
+        boardEntity.setBoardPass(boardDTO.getBoardPass());
+        boardEntity.setBoardTitle(boardDTO.getBoardTitle());
+        boardEntity.setBoardContents(boardDTO.getBoardContents());
+        boardEntity.setBoardHits(boardDTO.getBoardHits());
+        return boardEntity;
+    }
+}
