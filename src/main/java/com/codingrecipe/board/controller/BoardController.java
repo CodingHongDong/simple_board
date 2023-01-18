1933181975f44794ac86d867aef8edce59d6464d
HongDongHwan <hongdh94@gmail.com>
2023-01-18 15:54:58 +0900

    ADD board 페이징처리
    생각보다 로직이 까다롭다...  이해하자

--- src/main/java/com/codingrecipe/board/controller/BoardController.java
+++ src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -4,6 +4,9 @@ import com.codingrecipe.board.dto.BoardDTO;
 import com.codingrecipe.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -32,7 +35,7 @@ public class BoardController {
     }
 
     @GetMapping("/")
-    public String fondAll(Model model) {
+    public String findAll(Model model) {
         // DB에서 전체 게시글 데이터를 기져와서 list.html에 보여준다.
         List<BoardDTO> boardDTOList = boardService.findAll();
         model.addAttribute("boardList", boardDTOList);
@@ -41,7 +44,8 @@ public class BoardController {
     }
 
     @GetMapping("/{id}")
-    public String findById(@PathVariable Long id, Model model) {
+    public String findById(@PathVariable Long id, Model model,
+                           @PageableDefault(page = 1)Pageable pageable) {
         /*
             2가지 작업이 이루어져야 한다.
             1. 해당 게시글의 조회수를 하나 올리고
@@ -51,6 +55,8 @@ public class BoardController {
         BoardDTO boardDTO = boardService.findById(id);
 
         model.addAttribute("board", boardDTO);
+
+        model.addAttribute("page", pageable.getPageNumber());
 
         return "detail";
     }
@@ -75,6 +81,31 @@ public class BoardController {
     public String delete(@PathVariable Long id) {
         boardService.delete(id);
         return "redirect:/board/";
+    }
+
+    // /board/paging?page=1
+    @GetMapping("/paging")
+    public String paging(@PageableDefault(page = 1)Pageable pageable, Model model) {
+
+        Page<BoardDTO> boardList = boardService.paging(pageable);
+        int blockLimit = 3; // 보여지는 페이지 번호 갯수
+        int startPage = (((int)(Math.ceil((double)pageable.getPageNumber() / blockLimit))) -1) * blockLimit + 1; // 1 4 7 10 ~~
+        int endPage = ((startPage + blockLimit - 1) < boardList.getTotalPages()) ?
+                        startPage + blockLimit -1 : boardList.getTotalPages();
+
+        // page 갯수 20개
+        // 현재 사용자가 3페이지
+        // 1 2 3
+        // 현재 사용자가 7페이지
+        // 7 8 9
+        // 보여지는 페이지 갯수 3개
+
+        model.addAttribute("boardList", boardList);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
+        return "paging";
+
     }
 
 }
